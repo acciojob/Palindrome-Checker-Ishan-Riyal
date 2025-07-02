17d6217c0c2737ae4d2f2da24bd985b1a18da60f
Ishan Riyal <ishanriyal07@gmail.com>
2025-07-02 18:11:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-
+ const reverse = str.split('').reverse().join('');
+ return str === reverse ? true : false;
 }
+
 module.exports = palindrome
